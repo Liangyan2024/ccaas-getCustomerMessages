ed85efe1790ea4516fdbebfa5db899103eb41e99
Liangyan2024 <liang.yan@telusinternational.com>
2025-05-09 23:09:02 -0400

    Initial commit

---
+++ lambda_function.py
@@ -0,0 +1,25 @@
+import json
+import boto3
+
+
+dynamodb = boto3.resource('dynamodb')
+message_table = dynamodb.Table('ccaas-customerMessages')
+
+
+
+
+def lambda_handler(event, context):
+
+    contactFlow_name = 'async-chat-entry-QA'
+
+    contactFlow_messages = message_table.query(
+        IndexName = 'ContactFlowName-index',
+        KeyConditionExpression = key ('ContactFlowName').eq(contactFlow_name)
+    )
+
+    for message in contactFlow_messages['Items']:
+        print(message)
+
+    return 
+
+
