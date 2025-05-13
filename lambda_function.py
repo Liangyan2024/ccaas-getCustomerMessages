274926317bc978abb9ed1c3adc41da330f39a3a2
Liangyan2024 <liang.yan@telusinternational.com>
2025-05-12 20:48:07 -0400

    version 3

--- lambda_function.py
+++ lambda_function.py
@@ -1,24 +1,33 @@
 import json
 import boto3
+import os
 from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb')
 message_table = dynamodb.Table('ccaas-customerMessages')
-
-
+connect = boto3.client('connect')
+connect_instance_id = os.environ.get('Amazon_Connect_Instance_ID')
 
 
 def lambda_handler(event, context):
 
-    contactFlow_name = 'async-chat-entry-QA'
+    contactFlow_ID = event['Details']['ContactData']['Attributes'].get('ContactFlowID')
+
+    print (f'current_contact_flow_ID: {contactFlow_ID}')
 
     contactFlow_messages = message_table.query(
-        IndexName = 'ContactFlowName-index',
-        KeyConditionExpression = key('ContactFlowName').eq(contactFlow_name)
+        IndexName = 'ContactFlowID-index',
+        KeyConditionExpression = Key('ContactFlowName').eq(contactFlow_name)
     )
 
+  
+    messages = {}
+
     for message in contactFlow_messages['Items']:
-        print(message)
+        if message.get('MessageID'):
+            messages[message.get('MessageID')] = {'en_message': message.get('EnglishText', ''), 'fr_message': message.get('FrenchText', '')}
+
+    print (messages)
 
     return 
 
