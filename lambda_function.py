ddc640dde14cbbe7695d3d3c476f70cf110b4016
Liangyan2024 <liang.yan@telusinternational.com>
2025-05-14 00:05:40 -0400

    v7

--- lambda_function.py
+++ lambda_function.py
@@ -15,6 +15,10 @@ def lambda_handler(event, context):
      
     attrs = event['Details']['ContactData']['Attributes']
     language = attrs.get('language', 'en').lower()
+    
+    contact_id = event['Details']['ContactData'].get('ContactId')
+    
+
 
     print (f'current_contact_flow_name: {contactFlow_name}')
 
@@ -23,20 +27,49 @@ def lambda_handler(event, context):
         KeyConditionExpression = Key('ContactFlowName').eq(contactFlow_name)
     )
 
+    clear_existing_m_attributes(connect_instance_id, contact_id, attrs)
+
   
     messages = {}
 
     for message in contactFlow_messages['Items']:
         if message.get('MessageID'):
             if language == 'en':
-                messages[message.get('MessageID')] = message.get('EnglishText', '')
+                messages[f"m_{message.get('MessageID')}"] = message.get('EnglishText', '')
             else:
-                messages[message.get('MessageID')] = message.get('FrenchText', '')
+                messages[f"m_{message.get('MessageID')}"]  = message.get('FrenchText', '')
 
     print (messages)
+    
+    if messages:
+        connect.update_contact_attributes(
+        InstanceId=connect_instance_id,
+        InitialContactId=contact_id,
+        Attributes=messages
+        )   
+    
+    return  {
+        "statusCode": 200
+    }
 
-    return messages
 
+def clear_existing_m_attributes(instanceID, contactID, attributes):
 
+    m_attributes = {}
+
+    for item in attributes:
+        if item.startswith('m_'):
+            m_attributes[item] = ''
+
+    if m_attributes:
+        connect.update_contact_attributes(
+        InstanceId=instanceID,
+        InitialContactId=contactID,
+        Attributes=m_attributes
+        )
+
+    return {
+        "statusCode": 200
+    }
 
 
